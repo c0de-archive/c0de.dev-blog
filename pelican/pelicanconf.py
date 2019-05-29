eca962c47c57f314787fb93ee9543922a3d1d049
David Todd <c0de@c0defox.es>
2019-05-28 20:41:18 -0500

    finalize configuration

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -3,7 +3,8 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'c0de'
-SITENAME = "c0de's ongoing development"
+SITENAME = "c0de's development"
+SITESUBTITLE = "<i class=\"far fa-dollar-sign\"></i> cat /proc/c0de/thoughts"
 SITEURL = ''
 
 PATH = 'content'
@@ -20,12 +21,14 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Personal Site', 'https://c0defox.es'),)
+#LINKS = (('Personal Site', 'https://c0defox.es'),)
 
 # Social widget
-SOCIAL = (('Github', 'https://github.com/alopexc0de'),
-          ('Keybase', 'https://keybase.io/alopexc0de'),
-          ('Telegram', 'https://t.me/c0defox'),)
+SOCIAL = (('github', 'https://github.com/alopexc0de'),
+          ('keybase', 'https://keybase.io/alopexc0de'),
+          ('telegram', 'https://t.me/c0defox'),)
+
+SHARE = True
 
 DEFAULT_PAGINATION = 20
 
@@ -35,10 +38,31 @@ DEFAULT_PAGINATION = 20
 USE_FOLDER_AS_CATEGORY = True  # Use directories in content to specify the category
 DEFAULT_CATEGORY = 'none'
 
+# Theme settings
+
+THEME = "theme"
+THEME_STATIC_PATHS = ['static']
+CSS_FILE = "main.css"
+
+# Required settings to allow correct behavior of theme
+TAG_SAVE_AS = ""
+AUTHOR_SAVE_AS = ""
+DIRECT_TEMPLATES = ('index', 'categories', 'archives', 'past_readings', 'search', 'tipue_search')
+
+FOOTER = ("Content produced is &copy; <a href=\"mailto:c0de@c0de.dev\">c0de</a>. All rights reserved.<br />" +
+          "Code snippets on this site are, unless otherwise noted, released under " +
+          "<a href=\"https://opensource.org/licenses/MIT\" target=\"_license\">The MIT License</a>.")
+
+TWITTER_USERNAME = "foxyc0de"
+DISPLAY_PAGES_ON_MENU = False
+
+# Plugin paths and activated ones
+
 PLUGIN_PATHS = ['plugins/encrypt-content',
+                'plugins/upstream',
                 'plugins']
 
-PLUGINS = ['encrypt_content', 'deadlinks', 'bootstrapify']
+PLUGINS = ['encrypt_content', 'deadlinks', 'bootstrapify', 'tipue_search']
 
 # Plugin specific settings below
 
